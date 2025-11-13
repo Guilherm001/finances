8ca03526de570912d2e4237f356fba419f54c011
Guilherme <147566218+Guilherm001@users.noreply.github.com>
2025-11-13 12:55:53 +0000

    atualizando pagina de relatorios

--- src/components/Relatorios/index.tsx
+++ src/components/Relatorios/index.tsx
@@ -1,10 +1,43 @@
 'use client'
 
 export default function Relatorios() {
+
+    interface Relatorio {
+        id: number;
+        title: string;
+        amount: number;
+        category: string;
+        date: string;
+    }
+
+    const simulacaoRelatorios: Relatorio[] = [
+        {
+            id: 1,
+            title: 'Feijão',
+            amount: 9.50,
+            category: 'Despesas',
+            date: '20/10/2023'
+        },
+        {
+            id: 2,
+            title: 'Salario',
+            amount: 1500,
+            category: 'Renda',
+            date: '20/11/2023'
+        },
+        {
+            id: 3,
+            title: 'Vale Alimentação',
+            amount: 300,
+            category: 'Renda',
+            date: '22/11/2023'
+        }
+
+    ]
     
     return (
         <div>
-            <table>
+            <table className="w-full text-sm text-left text-gray-900">
                 <thead>
                     <tr>
                         <th>Titulo</th>
@@ -14,14 +47,15 @@ export default function Relatorios() {
                     </tr>
                 </thead>
                 <tbody>
+                        {simulacaoRelatorios.map(relatorio => (
                     <tr>
-                        <td>Relatório de Vendas</td>
-                        <td>Resumo das vendas mensais</td>
+                        <td>{relatorio.title}</td>
+                        <td>{relatorio.amount}</td>
+                        <td>{relatorio.category}</td>
+                        <td>{relatorio.date}</td>
                     </tr>
-                    <tr>
-                        <td>Relatório de Clientes</td>
-                        <td>Detalhes dos clientes cadastrados</td>
-                    </tr>
+                ))}
+                    
                 </tbody>
             </table>
         </div>
